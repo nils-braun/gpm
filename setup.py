401357ebf62fc0264e950b288078127b4cb464ff
Nils Braun <nils.braun@kit.edu>
2017-02-18 17:44:34 +0100

    Added setup files and requirements

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from setuptools import setup, find_packages
+
+
+def setup_package():
+    needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+    pytest_runner = ['pytest-runner'] if needs_pytest else []
+    setup(name="gpm",
+          version="0.1",
+          packages=find_packages(),
+          setup_requires=['setuptools>=17.1'] + pytest_runner,
+          tests_require=['pytest', 'pytest-cov', 'coverage'],
+          )
+
+if __name__ == "__main__":
+    setup_package()
+
