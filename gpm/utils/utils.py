f6af0c5319c66bdf459d750613f4a68484eedf6a
Nils Braun <nils.braun@kit.edu>
2017-02-18 19:05:49 +0100

    Added a convenience function to find the next gpm folder in the parents

--- gpm/utils/utils.py
+++ gpm/utils/utils.py
@@ -15,3 +15,17 @@ def get_results_file(base_dir):
 
 def get_config_file(base_dir):
     return os.path.join(get_gpm_dir(base_dir), "config")
+
+
+def find_next_gpm_base_dir(start_folder="."):
+    start_folder = os.path.abspath(start_folder)
+
+    if os.path.isdir(get_gpm_dir(start_folder)):
+        return start_folder
+
+    else:
+        parent_dir = os.path.abspath(os.path.join(start_folder, os.path.pardir))
+        if parent_dir == start_folder:
+            raise RuntimeError("Could not find a gpm base path (not in the parent folders).")
+
+        return find_next_gpm_base_dir(parent_dir)
