1bcf8f99f9b68e88fd077e04a9facd8f1ecf7608
Nils Braun <nils.braun@kit.edu>
2017-04-08 00:10:42 +0200

    Refactored the commits logic into the state

--- gpm/base/state.py
+++ gpm/base/state.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 
+from gpm.base.commit import store_commit, Commit
 from gpm.snapshots.snapshot_list import SnapshotList
 from gpm.db.database import Database
 from gpm.snapshots.creator import create_snapshot
@@ -17,8 +18,13 @@ class State:
 
     @contextmanager
     def get_calculation(self):
-        calculation = Calculation(self.snapshots, self.db)
+        calculation = Calculation()
 
-        calculation.start()
         yield calculation
-        calculation.end()
+
+        for output_file in calculation.output_files:
+            output_file_snapshot = create_snapshot(output_file)
+            dependency_snapshots = self.snapshots
+
+            commit = Commit(output_file_snapshot, dependency_snapshots)
+            store_commit(commit, self.db)
