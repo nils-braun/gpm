cead4fba6144cf369e204b4ebefa2fe9857cae66
Nils Braun <nils.braun@kit.edu>
2017-04-08 11:01:59 +0200

    Implemented a faster key value db based on files and folders

--- gpm/db/key_value_db.py
+++ gpm/db/key_value_db.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 try:
     import cPickle as pickle
 except ImportError:
@@ -9,34 +11,47 @@ import os
 def get_db_file_path(folder):
     if not folder:
         folder = os.getcwd()
-    return os.path.join(folder, ".gpm/state.db")
+    return os.path.join(folder, ".gpm/states/")
 
 
 class KeyValueDB:
     def __init__(self, db_folder):
         self.db_file_path = get_db_file_path(db_folder)
-        self._dict = {}
+        self._dict = defaultdict(dict)
 
-    def _load(self):
-        if not os.path.exists(self.db_file_path):
-            self._dict = {}
-        else:
-            with open(self.db_file_path, "rb") as f:
-                self._dict = pickle.load(f)
+    def _load(self, stub):
+        db_file_path = self._get_filename(stub)
+        if os.path.exists(db_file_path):
+            with open(db_file_path, "rb") as f:
+                self._dict[stub] = pickle.load(f)
 
-    def _save(self):
-        if not os.path.exists(self.db_file_path):
-            basename, filename = os.path.split(self.db_file_path)
-            os.makedirs(basename)
+    def _save(self, stub):
+        db_file_path = self._get_filename(stub)
 
-        with open(self.db_file_path, "wb") as f:
-            pickle.dump(self._dict, f)
+        if not os.path.exists(db_file_path):
+            basename, filename = os.path.split(db_file_path)
+            if basename and not os.path.exists(basename):
+                os.makedirs(basename)
+
+        with open(db_file_path, "wb") as f:
+            pickle.dump(self._dict[stub], f)
+
+    @staticmethod
+    def _get_stub(key):
+        return key[-2:]
+
+    def _get_filename(self, stub):
+        return os.path.join(self.db_file_path, stub + ".db")
 
     def __getitem__(self, item):
-        self._load()
-        return self._dict[item]
+        stub = self._get_stub(item)
+
+        self._load(stub)
+        return self._dict[stub][item]
 
     def __setitem__(self, key, value):
-        self._load()
-        self._dict[key] = value
-        self._save()
+        stub = self._get_stub(key)
+
+        self._load(stub)
+        self._dict[stub][key] = value
+        self._save(stub)
