5aebe279edc5396cbfd7fafabab94040359b3ce9
Nils Braun <nils.braun@kit.edu>
2017-04-09 11:29:27 +0200

    Make it required to implement an __eq__ operation on snapshots

--- gpm/snapshots/base.py
+++ gpm/snapshots/base.py
@@ -10,3 +10,6 @@ class BaseSnapshot:
     def name(self):
         return "{class_name}: {name_param}".format(class_name=self.__class__.__name__,
                                                    name_param=self.name_param)
+
+    def __eq__(self, other):
+        raise NotImplementedError
