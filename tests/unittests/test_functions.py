a268756335f971d2dfbea95b68bee850933700dc
Nils Braun <nils.braun@kit.edu>
2017-04-09 11:11:02 +0200

    Added a unit test for some free functions

---
+++ tests/unittests/test_functions.py
@@ -0,0 +1,19 @@
+from gpm.fixtures import IntegrationTestFixture
+
+from gpm.snapshots.creator import create_snapshot
+from gpm.snapshots.file import FileSnapshot
+
+
+class FunctionsTestCase(IntegrationTestFixture):
+    def test_create_snapshot(self):
+        with open(self.file_name, "w") as f:
+            f.write("test")
+
+        file_snapshot = create_snapshot(self.file_name)
+        self.assertIsInstance(file_snapshot, FileSnapshot)
+
+        created_file_snapshot = FileSnapshot(self.file_name)
+        file_snapshot = create_snapshot(created_file_snapshot)
+        self.assertIs(file_snapshot, created_file_snapshot)
+
+        self.assertRaises(AttributeError, create_snapshot, "another_file.py")
